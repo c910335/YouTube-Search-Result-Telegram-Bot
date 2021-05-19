334a2e4491773243c8d3c41d6d8d2db5bdcfeb33
Tatsujin Chin <c910335@gmail.com>
2021-05-20 03:22:39 +0800

    fix retry when telegram connection error

--- src/bot.rb
+++ src/bot.rb
@@ -30,92 +30,97 @@ class Bot
       @bot = bot
       set_update
       log 'Bot started.'
-      bot.listen do |msg|
-        log msg
-        case msg
-        when Telegram::Bot::Types::Message
-          case msg.text
-          when '/start'
-            reply_to(msg, 'hello, world')
-          when '/sub'
-            reply_to(msg, 'Please enter the keyword you would like to subscribe.',
-                     reply_markup: Telegram::Bot::Types::ForceReply.new(force_reply: true))
-          when '/unsub'
-            reply_to(msg, 'Please enter the keyword you would like to unsubscribe.',
-                     reply_markup: Telegram::Bot::Types::ForceReply.new(force_reply: true))
-          when '/list'
-            if (user_subs = subs[msg.chat.id]) && !user_subs.empty?
-              reply_to(msg, "You currently subscribe to\n#{user_subs.keys.join("\n")}")
+      begin
+        bot.listen do |msg|
+          log msg
+          case msg
+          when Telegram::Bot::Types::Message
+            case msg.text
+            when '/start'
+              reply_to(msg, 'hello, world')
+            when '/sub'
+              reply_to(msg, 'Please enter the keyword you would like to subscribe.',
+                       reply_markup: Telegram::Bot::Types::ForceReply.new(force_reply: true))
+            when '/unsub'
+              reply_to(msg, 'Please enter the keyword you would like to unsubscribe.',
+                       reply_markup: Telegram::Bot::Types::ForceReply.new(force_reply: true))
+            when '/list'
+              if (user_subs = subs[msg.chat.id]) && !user_subs.empty?
+                reply_to(msg, "You currently subscribe to\n#{user_subs.keys.join("\n")}")
+              else
+                reply_to(msg, 'Subscription not found.')
+              end
+            when '/update'
+              update(clear: false)
             else
-              reply_to(msg, 'Subscription not found.')
-            end
-          when '/update'
-            update(clear: false)
-          else
-            if replied = msg.reply_to_message
-              case replied.text
-              when 'Please enter the keyword you would like to subscribe.'
-                if sub(msg.text, msg.chat.id, msg.from.username)
-                  reply_to(msg, "Subscribed to \"#{msg.text}\" successfully.")
-                else
-                  reply_to(msg, "You have already subscribed to \"#{msg.text}\".")
-                end
-              when 'Please enter the keyword you would like to unsubscribe.'
-                if unsub(msg.text, msg.chat.id)
-                  reply_to(msg, "Unsubscribed to \"#{msg.text}\" successfully.")
-                else
-                  reply_to(msg, 'Subscription not found.')
+              if replied = msg.reply_to_message
+                case replied.text
+                when 'Please enter the keyword you would like to subscribe.'
+                  if sub(msg.text, msg.chat.id, msg.from.username)
+                    reply_to(msg, "Subscribed to \"#{msg.text}\" successfully.")
+                  else
+                    reply_to(msg, "You have already subscribed to \"#{msg.text}\".")
+                  end
+                when 'Please enter the keyword you would like to unsubscribe.'
+                  if unsub(msg.text, msg.chat.id)
+                    reply_to(msg, "Unsubscribed to \"#{msg.text}\" successfully.")
+                  else
+                    reply_to(msg, 'Subscription not found.')
+                  end
                 end
               end
             end
-          end
-        when Telegram::Bot::Types::CallbackQuery
-          begin
-            case msg.data
-            when /^add \S+ .+/
-              add_video(msg)
-            when 'discard'
-              bot.api.edit_message_text(
-                chat_id: msg.message.chat.id,
-                message_id: msg.message.message_id,
-                text: msg.message.text,
-                disable_web_page_preview: true,
-                reply_markup: nil
-              )
-            when /^clear .+/
-              query = msg.data[/^clear (.+)$/, 1]
-              bot.api.send_message(
-                chat_id: msg.message.chat.id,
-                text: "Are you sure to clear the \"#{query}\" playlist?",
-                reply_markup: Telegram::Bot::Types::InlineKeyboardMarkup.new(
-                  inline_keyboard: [[
-                    Telegram::Bot::Types::InlineKeyboardButton.new(
-                      text: 'Clear', callback_data: "force_clear #{query}"
-                    ),
-                    Telegram::Bot::Types::InlineKeyboardButton.new(
-                      text: 'No', callback_data: 'no_clear'
-                    )
-                  ]]
+          when Telegram::Bot::Types::CallbackQuery
+            begin
+              case msg.data
+              when /^add \S+ .+/
+                add_video(msg)
+              when 'discard'
+                bot.api.edit_message_text(
+                  chat_id: msg.message.chat.id,
+                  message_id: msg.message.message_id,
+                  text: msg.message.text,
+                  disable_web_page_preview: true,
+                  reply_markup: nil
                 )
-              )
-            when /^force_clear .+/
-              query = msg.data[/^force_clear (.+)$/, 1]
-              bot.api.edit_message_text(
-                chat_id: msg.message.chat.id,
-                message_id: msg.message.message_id,
-                text: "The \"#{query}\" playlist has been cleared.",
-                reply_markup: nil
-              )
-              subs[msg.message.chat.id][query] =
-                youtube.clear_playlist(subs[msg.message.chat.id][query], msg.from.username, query)
-            when 'no_clear'
-              bot.api.delete_message(chat_id: msg.message.chat.id, message_id: msg.message.message_id)
+              when /^clear .+/
+                query = msg.data[/^clear (.+)$/, 1]
+                bot.api.send_message(
+                  chat_id: msg.message.chat.id,
+                  text: "Are you sure to clear the \"#{query}\" playlist?",
+                  reply_markup: Telegram::Bot::Types::InlineKeyboardMarkup.new(
+                    inline_keyboard: [[
+                      Telegram::Bot::Types::InlineKeyboardButton.new(
+                        text: 'Clear', callback_data: "force_clear #{query}"
+                      ),
+                      Telegram::Bot::Types::InlineKeyboardButton.new(
+                        text: 'No', callback_data: 'no_clear'
+                      )
+                    ]]
+                  )
+                )
+              when /^force_clear .+/
+                query = msg.data[/^force_clear (.+)$/, 1]
+                bot.api.edit_message_text(
+                  chat_id: msg.message.chat.id,
+                  message_id: msg.message.message_id,
+                  text: "The \"#{query}\" playlist has been cleared.",
+                  reply_markup: nil
+                )
+                subs[msg.message.chat.id][query] =
+                  youtube.clear_playlist(subs[msg.message.chat.id][query], msg.from.username, query)
+              when 'no_clear'
+                bot.api.delete_message(chat_id: msg.message.chat.id, message_id: msg.message.message_id)
+              end
+              bot.api.answer_callback_query(callback_query_id: msg.id)
+            rescue Telegram::Bot::Exceptions::ResponseError, NoMethodError => e
+              log e.message
             end
-            bot.api.answer_callback_query(callback_query_id: msg.id)
-          rescue Telegram::Bot::Exceptions::ResponseError, NoMethodError => e
-            log e.message
           end
         end
+      rescue Faraday::ConnectionFailed => e
+        log e.message
+        retry
       end
     end
   end
